d914f24af7d8047636a7a0cdba59f943b6a33591
Herbert V <herbertvargas@gmail.com>
2018-08-10 22:41:04 -0500

    add authentication with devise, create default user, add some links to improve browsing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,17 @@ def hello
     render html: "Bienvenido a NiUnaMas"
     
 end
+    #To protect the app from hackers
+    protect_from_forgery with: :exception
+    
+    #this request authentication before load any page or better before any action
+    before_action :authenticate_user!
+    #the page landing does not require authentication
+    skip_before_action :authenticate_user!, :only => [:landing]
+
+#what to do after sign in
+def after_sign_in_path_for(resource)
+    admin_pages_dashboard_url
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'admin_pages/dashboard'
   get 'static_pages/landing'
  
