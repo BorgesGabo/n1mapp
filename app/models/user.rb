eb8e3a330c3e4839a997285bcc57f86b176e9f9c
Herbert V <herbertvargas@gmail.com>
2018-09-25 17:20:58 -0500

    new query to be used in admin panel

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :aggression_details, dependent: :destroy
+  def self.get_details
+    self.joins(:aggression_details).select("name,surname,district,aggression_date,aggression_time,scene,mechanism,aggression_details.id")
+  end
+
 end
